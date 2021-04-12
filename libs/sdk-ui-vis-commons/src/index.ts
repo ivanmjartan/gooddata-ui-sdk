894d3ab9a2470aabe777745141cee5c5c4d585dc
Petr Benes <petr.benes@gooddata.com>
2021-04-12 15:19:45 +0200

    Responsive popup legend for each chart.
    Add new tag "no-plug-viz-tests" for generating storybook scenarios
    
    RELATED: ONE-4920 Popup Legend

--- libs/sdk-ui-vis-commons/src/index.ts
+++ libs/sdk-ui-vis-commons/src/index.ts
@@ -32,10 +32,6 @@ export {
 
 export { IColorMapping } from "./coloring/types";
 
-//
-//
-//
-
 export { Legend, ILegendProps } from "./legend/Legend";
 
 export { StaticLegend, IStaticLegendProps } from "./legend/StaticLegend";
@@ -46,7 +42,7 @@ export { HeatmapLegend, IHeatmapLegendProps } from "./legend/HeatmapLegend";
 
 export { ColorLegend, IColorLegendProps } from "./legend/ColorLegend";
 
-export { IPagingProps, Paging } from "./legend/Paging";
+export { IPagingProps, ButtonsOrientationType, Paging } from "./legend/Paging";
 
 export { IHeadlinePaginationProps, HeadlinePagination } from "./compactSize/HeadlinePagination";
 
@@ -66,6 +62,7 @@ export {
     IRange,
     LegendOptionsItemType,
     ItemBorderRadiusPredicate,
+    IHeatmapLegendSize,
 } from "./legend/types";
 
 export { fixEmptyHeaderItems } from "./utils/fixEmptyHeaderItems";

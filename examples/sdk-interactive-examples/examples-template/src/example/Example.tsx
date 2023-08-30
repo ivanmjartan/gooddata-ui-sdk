05c472fe6d7f5a31000f213e33caf7ee74f175ac
ivanmjartan <ivan.mjartan@gooddata.com>
2023-08-30 13:25:58 +0200

    chore: migrate interactive examples into SDK
    JIRA: RAIL-5122

---
+++ examples/sdk-interactive-examples/examples-template/src/example/Example.tsx
@@ -0,0 +1,11 @@
+// (C) 2021 GoodData Corporation
+import React from "react";
+
+/*
+import * as Catalog from "../catalog.js";
+import Hint from "../Hint.js";
+*/
+
+export default () => {
+    return <>this is a example template</>;
+};
